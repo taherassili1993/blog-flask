9eff52ee009d4ba559a66a9f52ff387fd6b04091
taherassili1993 <assili.tahir@gmail.com>
2023-12-01 13:18:41 +0100

    add table users avec pymysql

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, render_template
+import pymysql
+from database import db
 app = Flask(__name__)
 @app.route('/')
 def welcome():
+    db_config = db.db_config
+
+    db_conn = pymysql.connect(**db_config)
+    cursor = db_conn.cursor()
+
+    sql = db.sql
+ 
+    cursor.execute(sql)
     return render_template("index.html");
 app.run()
